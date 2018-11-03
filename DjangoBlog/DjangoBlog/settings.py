76fe194deb5062af333c4f3d1609586d1fe74c46
ninjiacoder <ninjiacoder@gmail.com>
2018-11-03 19:57:22 +0800

    :sparkles: 新增分页功能

--- DjangoBlog/DjangoBlog/settings.py
+++ DjangoBlog/DjangoBlog/settings.py
@@ -42,7 +42,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'pure_pagination',
 ]
+
+PAGINATION_SETTINGS = {
+    'PAGE_RANGE_DISPLAYED': 3,
+    'MARGIN_PAGES_DISPLAYED': 2,
+
+    'SHOW_FIRST_PAGE_WHEN_INVALID': True,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- DjangoBlog/blog/views.py
+++ DjangoBlog/blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.views import View
 from blog.models import Blog, Category, Tag
+from pure_pagination import PageNotAnInteger, Paginator
 
 # Create your views here.
 
 class IndexView(View):
     def get(self, request):
         all_blogs = Blog.objects.all().order_by('-id')
+
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        p = Paginator(all_blogs, 5, request=request)
+        all_blogs = p.page(page)
+
         return render(request, 'index.html', {
             'all_blogs': all_blogs,
         })
