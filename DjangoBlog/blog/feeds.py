0ab45392d597d2db832b12cdb7d2ef61496ad9e2
ninjiacoder <ninjiacoder@gmail.com>
2018-11-04 14:11:18 +0800

    :sparkles: RSS 订阅中的文章支持 Markdown 渲染

--- DjangoBlog/blog/feeds.py
+++ DjangoBlog/blog/feeds.py
@@ -1,6 +1,7 @@
 from django.contrib.syndication.views import Feed
 from django.urls import reverse
 from blog.models import Blog
+import markdown2
 
 class BlogRssFeed(Feed):
     title = "忍者小屋"
@@ -10,6 +11,7 @@ class BlogRssFeed(Feed):
     def item_title(self, item):
         return item.title
     def item_description(self, item):
+        item.content = markdown2.markdown(item.content, extras=['fenced-code-blocks'])
         return item.content
     def item_link(self, item):
         return reverse('blog_id', args=[item.id,])
